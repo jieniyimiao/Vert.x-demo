bdfde9ad29d0d2ed37c61ab2930d01e81e2871fc
caogu <90433899@qq.com>
2019-08-02 10:17:36 +0800

    [Bug/Story ID]： [Bug/Story概述]： [修改描述]：增加超时时间和Header [修改人]： [审核人]：

--- src/main/java/com/sinosun/ctrip/client/CtripClient.java
+++ src/main/java/com/sinosun/ctrip/client/CtripClient.java
@@ -21,25 +21,32 @@ public class CtripClient {
 
     public void getTicketPriceFromCtrip(JSONObject req, RoutingContext routingContext) {
         logger.info("getTicketPrice HTTP request  with data : " + req);
-        HttpClient.getHttpClient().postAbs(GET_TICKET_PRICE_URL).sendJson(req, resp -> {
-            if (resp.succeeded()) {
-                HttpResponse<Buffer> response = resp.result();
-                JsonObject ret = response.bodyAsJsonObject();
-                logger.info("getTicketPrice HTTP response with body : " + ret);
+        HttpClient.getHttpClient()
+                .postAbs(GET_TICKET_PRICE_URL)
+                .timeout(5000)
+                .putHeader("user-agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36")
+                .putHeader("origin", "https://m.ctrip.com")
+                .putHeader("referer", "https://m.ctrip.com/webapp/train/?secondwakeup=true&dpclickjump=true&from=https%3A%2F%2Fm.ctrip.com%2Fhtml5%2F")
+                .sendJson(req, resp -> {
+                    if (resp.succeeded()) {
+                        HttpResponse<Buffer> response = resp.result();
+                        JsonObject ret = response.bodyAsJsonObject();
+                        logger.info("getTicketPrice HTTP response with body : " + ret);
 
-                JsonObject responseStatus = ret.getJsonObject("ResponseStatus");
-                if (!"Success".equals(responseStatus.getString("Ack"))) {
-                    logger.error("get ticket price from ctrip failed : " + responseStatus.toString());
-                    routingContext.response().setStatusCode(400).end();
-                } else {
-                    JsonObject responseBody = ret.getJsonObject("ResponseBody");
-                    routingContext.response().putHeader("content-type", "application/json; charset=utf-8")
-                            .end(responseBody.toString());
-                }
-            } else {
-                logger.error("HTTP error : " + resp.cause());
-            }
-        });
+                        JsonObject responseStatus = ret.getJsonObject("ResponseStatus");
+                        if (!"Success".equals(responseStatus.getString("Ack"))) {
+                            logger.error("get ticket price from ctrip failed : " + responseStatus.toString());
+                            routingContext.response().setStatusCode(400).end();
+                        } else {
+                            JsonObject responseBody = ret.getJsonObject("ResponseBody");
+                            routingContext.response().putHeader("content-type", "application/json; charset=utf-8")
+                                    .end(responseBody.toString());
+                        }
+                    } else {
+                        logger.error("HTTP error : " + resp.cause());
+                        routingContext.response().setStatusCode(400).end();
+                    }
+                });
     }
 
 }
